bc9f998c27b030b21a719060ab4385d8f6d4cb76
qzmlgfj <qzmlgfj@gmail.com>
2023-07-12 12:22:31 +0800

    顶部菜单栏逻辑调整，变为"未回复-已回复-全部"循环

--- frontend/src/components/FootBar.vue
+++ frontend/src/components/FootBar.vue
@@ -40,7 +40,7 @@ export default {
                     });
                 } else {
                     this.$store.commit("clearUserName");
-                    this.$store.commit("setQueryMode", "answered");
+                    this.$store.commit("setQueryMode", "unprivate_and_answered");
                     this.$store.commit("updateQuestion");
                 }
                 this.clickTimes = 0;
